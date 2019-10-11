5b2266435352364b1e0d57e231f1b79b2b4188e1
Andre Lucas de Sousa Pinto <andrelucax@gmail.com>
2019-10-11 13:14:53 -0300

    Adding password reset
    Co-authored-by: Ivan Diniz Dobbin <ivandinizdobbin2@gmail.com>

--- monitoria/urls.py
+++ monitoria/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('monitoria.auth_urls')),
+    url('^', include('django.contrib.auth.urls')),
 ]
