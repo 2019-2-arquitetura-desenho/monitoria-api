c136b19260b867a3c88719f7ac1aa666628d47f4
Andre Lucas de Sousa Pinto <andrelucax@gmail.com>
2019-10-15 18:22:36 -0300

    Adding a way to use static files in production

--- monitoria/settings.py
+++ monitoria/settings.py
@@ -22,7 +22,7 @@ DOMAIN = env('DOMAIN')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # Application definition
@@ -127,6 +127,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'monitoria.urls'
