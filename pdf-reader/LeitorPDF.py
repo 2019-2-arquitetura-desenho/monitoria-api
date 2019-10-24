6206b0adb8e913c1298f3deb2479c7c338dcfa78
bielrossi15 <bielrossiborba@gmail.com>
2019-10-24 12:36:49 -0300

    Adicionando extractor de materias
    Co-authored-by: João Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Paulo Vitor <paulovitorrocha.unb@gmail.com>

--- pdf-reader/LeitorPDF.py
+++ pdf-reader/LeitorPDF.py
@@ -50,6 +50,41 @@ class RegExtractor(Extractor):
         return reg
 
 
+class SubExtractor(Extractor):
+
+    def extract(self):
+        # creating regex
+        pattern1 = r'\d{6}  \w'
+        pattern2 = r'\d{6}  \w.+'
+        search = re.compile(pattern1)
+        search2 = re.compile(pattern2)
+        # opening pdf file
+        with open(self.reg_pdf, 'rb') as f:
+            sub_extractor = pdftotext.PDF(f)
+
+            # finding pattern1
+            sub = search.findall("\n\n".join(sub_extractor))
+            # finding subject codes
+            sub_code = re.findall(r"\d{6}", "".join(sub))
+
+            # finding pattern1
+            sub2 = search2.findall("\n\n".join(sub_extractor))
+            # finding subject grades
+            sub_grade = re.findall(" [A-Z]{2} ", "".join(sub2))
+
+            subs = [[sub_code[i], sub_grade[i]] for i in range(0, len(sub))]
+
+            possible = [' MM ', ' MS ', ' SS ']
+
+            sub_res = [i for i in subs if i[1] in possible]
+            for i in sub_res:
+                i[1] = i[1].replace(' ', '')
+            sub_res = tuple(sub_res)
+            # print(sub_res)
+
+        return sub_res
+
+
 def code(pdf_extractor: Extractor):
     pdf_extractor.extract()
 
@@ -59,3 +94,4 @@ if __name__ == "__main__":
     pdf_test = './tmp.pdf'
     code(IraExtractor(pdf_test))
     code(RegExtractor(pdf_test))
+    code(SubExtractor(pdf_test))
