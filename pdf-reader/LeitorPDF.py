ca03533fbc2cabe7ba5e913923a246fbf8205133
bielrossi15 <bielrossiborba@gmail.com>
2019-10-23 23:46:52 -0300

    Adicionando funcionalidade para leitura da matricula
    Co-authored-by: João Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Paulo Vitor <paulovitorrocha.unb@gmail.com>

--- pdf-reader/LeitorPDF.py
+++ pdf-reader/LeitorPDF.py
@@ -22,9 +22,23 @@ class PdfExtractor:
 
         return ira
 
+    def read_reg(self):
+        # creating regex
+        pattern = '(\d{2}/\d{7})'
+        search = re.compile(pattern)
+        # opening pdf file
+        with open(self.reg_pdf, 'rb') as f:
+            reg_extractor = pdftotext.PDF(f)
+            reg = search.findall("\n\n".join(reg_extractor))
+            reg = reg[0]
+            print(reg)
+
+        return reg
+
 
 if __name__ == "__main__":
     
     pdf_test = './tmp.pdf'
     obj = PdfExtractor(pdf_test)
     obj.read_IRA()
+    obj.read_reg()
