fc05b1f1a3b1c8afc6985796d2a14ac5f7ff2d74
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2019-10-24 20:29:10 -0300

    Fixing PDFExtrator
    Co-authored-by: João Rossi <bielrossiborba@gmail.com>
    Co-authored-by: Paulo Vitor <paulovitorrocha.unb@gmail.com>
    Co-authored-by: Andre Lucas de Sousa Pinto <andrelucax@gmail.com>

--- pdf-reader/LeitorPDF.py
+++ pdf-reader/LeitorPDF.py
@@ -32,11 +32,28 @@ class PDFExtractor(Extractor):
         # creating regex
         pattern = r'\d\.\d+'
         search = re.compile(pattern)
+        # opening pdf file
+        with open(self.reg_pdf, 'rb') as f:
+            IRA_extractor = pdftotext.PDF(f)
+            ira = search.findall("\n\n".join(IRA_extractor))
+            ira = ira[0]
+            # print()
+            # print(ira)
+
+        return ira
 
     def extractReg(self):
         # creating regex
         pattern = r'(\d{2}/\d{7})'
         search = re.compile(pattern)
+        # opening pdf file
+        with open(self.reg_pdf, 'rb') as f:
+            reg_extractor = pdftotext.PDF(f)
+            reg = search.findall("\n\n".join(reg_extractor))
+            reg = reg[0]
+            # print(reg)
+
+        return reg
 
     def extractSub(self):
         # creating regex
@@ -44,6 +61,31 @@ class PDFExtractor(Extractor):
         pattern2 = r'\d{6}  \w.+'
         search = re.compile(pattern1)
         search2 = re.compile(pattern2)
+        # opening pdf file
+        with open(self.reg_pdf, 'rb') as f:
+            sub_extractor = pdftotext.PDF(f)
+
+            # finding pattern1
+            sub = search.findall("\n\n".join(sub_extractor))
+            # finding subject codes
+            sub_code = re.findall(r"\d{6}", "".join(sub))
+
+            # finding pattern1
+            sub2 = search2.findall("\n\n".join(sub_extractor))
+            # finding subject grades
+            sub_grade = re.findall(" [A-Z]{2} ", "".join(sub2))
+
+            subs = [[sub_code[i], sub_grade[i]] for i in range(0, len(sub))]
+
+            possible = [' MM ', ' MS ', ' SS ']
+
+            sub_res = [i for i in subs if i[1] in possible]
+            for i in sub_res:
+                i[1] = i[1].replace(' ', '')
+            sub_res = tuple(sub_res)
+            # print(sub_res)
+
+        return sub_res
 
 def extract_code(pdf_extractor: Extractor):
     pdf_extractor.template_method()
